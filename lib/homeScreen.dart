821905a047b05c9660783dff46f1b8d4532eaceb
Iftekhar Ahmed <iftekharahmed728372@gmail.com>
2023-02-26 02:03:45 +0500

    Getx App Final Commit

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -11,28 +11,86 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final heigh=MediaQuery.of(context).size.height * 1 ;
     return Scaffold(
       appBar: AppBar(
         title: Text('App Using getx'),
         centerTitle: true,
-
       ),
       body: Column(
         children: [
-
+          Card(
+            child: ListTile(
+              title: Text('Getx Dialog Box'),
+              subtitle: Text('Click MeGetx Dialog Alert with Default Dialoge'),
+              onTap: () {
+                Get.defaultDialog(
+                    title: 'Delete Chat',
+                    content: Card(
+                      child: Column(
+                        children: [
+                          Text('Are You sure you want to exit')],
+                      ),
+                    ),
+                    confirm: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Yes')),
+                    cancel:
+                        TextButton(onPressed: () {}, child: Text('Cancel')));
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Getx Bottom Sheet'),
+              subtitle: Text('Click Me! To change to Dark or white or system background color'),
+              onTap: (){
+                Get.bottomSheet(
+                 Container(
+                   height: heigh*.5,
+                   // height: Get.height*.45,
+                   width: 400,
+                   decoration: BoxDecoration(
+                     color: Colors.teal,
+                     borderRadius: BorderRadius.circular(30)
+                   ),
+                   child: Column(
+                     children: [
+                       ListTile(
+                         leading: Icon(Icons.light_mode_outlined,color: Colors.white,),
+                         title: Text('Light mode'),
+                         onTap: (){
+                           Get.changeTheme(ThemeData.light());
+                         },
+                       ),
+                       ListTile(
+                         leading: Icon(Icons.dark_mode,color: Colors.white70,),
+                         title: Text('Dark mode'),
+                         onTap: (){
+                           Get.changeTheme(ThemeData.dark());
+                         },
+                       )
+                     ],
+                   ),
+                 )
+                );
+              },
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        child: Icon(Icons.cached_sharp),
+        onPressed: () {
           Get.snackbar(
-              icon:Icon(Icons.account_circle_sharp),
-              'Iftekhar Ahmed', 'Thanks for using my App',
-          backgroundColor: Colors.red
-          );
-
+              icon: Icon(Icons.account_circle_sharp),
+              'Iftekhar Ahmed',
+              'Thanks for using my App',
+              backgroundColor: Colors.red);
         },
       ),
-
     );
   }
 }
