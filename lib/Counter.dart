b56fddaa24838540f9d4f6914cb37decbd2c042a
Iftekhar Ahmed <iftekharahmed728372@gmail.com>
2023-02-27 21:37:26 +0500

    Getx App Counter.dart completed  Commit

--- lib/Counter.dart
+++ lib/Counter.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'counter_controller.dart';
 
 class CounterApp extends StatefulWidget {
   const CounterApp({Key? key}) : super(key: key);
@@ -9,7 +13,9 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
+
   int x=0;
+  CounterController controller= Get.put(CounterController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +26,28 @@ class _CounterAppState extends State<CounterApp> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-         Center(child: Text(x.toString()),)
+         Center(child: Text("Counter Using simple initState = "+x.toString()),),
+          SizedBox(height: 14,),
+          Center(child: Text('Counter Using Getx'),),
+
+
+          TextButton(
+            onPressed: () {
+              controller.incrementCounter();
+
+            },
+            child: Icon(
+              semanticLabel: 'GetxCounter' ,
+              size: 30 ,
+              Icons.add,
+            )
+
+          ),
+          Center(
+          child:Obx((){
+            return Text(controller.counter.toString());
+    }) ,)
+
           
         ],
       ),
